be073cadccc901652e3fc0112aa343d93f7bf3a6
Muhammed Reşid Ercan <muhammedresidercan@gmail.com>
2022-12-20 23:54:15 +0300

    20/12/22

---
+++ src/test/java/day03_Example/Day03_ExampleCSS.java
@@ -0,0 +1,19 @@
+package day03_Example;
+
+public class Day03_ExampleCSS {
+
+    public static void main(String[] args) {
+
+        /*
+    -Amazon sayfasına gidiniz
+    -Arama kutusunu locate ediniz
+    -Arama kutusunun tagName'inin input olduğunu test ediniz
+    -Arama kutusunun name atributu'nun değerinin field-keywords olduğunu test ediniz
+    -Sayfayı kapatınız
+        */
+
+
+
+
+    }
+}
